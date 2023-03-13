b6aaec4baed90a33a8c57319ab189b6082ac4f82
Gowtham <49744195+gowtham0210@users.noreply.github.com>
2023-03-13 17:02:15 +0530

    basic_project_created

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Gowtham Bujju on 13/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
